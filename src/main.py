072ba47b1e783fa8abb7b0ba4074b3d273fa8d34
Isaac Berlin <isaacmorrisberlin@gmail.com>
2022-03-14 17:13:36 -0500

    added new file for main functions, started dynamic sizing for different length words

--- src/main.py
+++ src/main.py
@@ -1,51 +1,37 @@
-import tkinter as tk
+from tkinter import *
 import random
+import math
+from core import *
 
-WORD = "hello"
+WORD = "7"
+PADDING_CALCULATION_X = (abs(500 - (76 * len(WORD))))//(len(WORD) * 2)
 
-
-class Cube:
-    def __init__(self, row: int, position: int, correct_word: str):
-        self.row = row
-        self.position = position
-        self.correct_word = correct_word
-        self.correct_letter = correct_word[position]
-        self.color = "white"
-
-    def check(self, guess: str):
-        if guess[self.position] not in self.correct_word:
-            self.color = "grey"
-        elif guess[self.position] in self.correct_word and guess[self.position] != self.correct_letter:
-            self.color = "yellow"
-        elif guess[self.position] == self.correct_letter:
-            self.color = "green"
-
-
-def make_row(row_num: int, length: int, correct_word: str) -> list:
-    row = []
-    for i in range(length):
-        row_cube = Cube(row_num, i, correct_word)
-        row.append(row_cube)
-    return row
-
-
-def make_columns(num_of_col: int, length_of_rows: int, correct_word: str) -> list:
-    arr = []
-    for i in range(num_of_col):
-        arr.append(make_row(i, length_of_rows, correct_word))
-    return arr
-
-
-def test_row(object_to_test: list, row_num: int, word_to_test: str):
-    for i in range(row_num):
-        object_to_test[row_num - 1][i].check(word_to_test)
-
-
-window = tk.Tk()
+window = Tk()
 window.title("Wordle")
 window.configure(background="light grey")
-
 window.geometry("500x500")
+window.minsize(500, 500)
+window.maxsize(500, 500)
+
+
+test_label = Label(text="", width=9, height=4)
+test_label.grid(column=0, row=0, padx=(PADDING_CALCULATION_X, PADDING_CALCULATION_X), pady=(100, 10))
+
+
+test_label2 = Label(text="B", width=9, height=4)
+test_label2.grid(column=1, row=0, padx=(PADDING_CALCULATION_X, PADDING_CALCULATION_X), pady=(100, 10))
+
+
+test_label3 = Label(text="C", width=9, height=4)
+test_label3.grid(column=2, row=0, padx=(PADDING_CALCULATION_X, PADDING_CALCULATION_X), pady=(100, 10))
+
+
+test_label4 = Label(text="D", width=9, height=4)
+test_label4.grid(column=3, row=0, padx=(PADDING_CALCULATION_X, PADDING_CALCULATION_X), pady=(100, 10))
+
+
+test_label5 = Label(text="E", width=9, height=4)
+test_label5.grid(column=4, row=0, padx=(PADDING_CALCULATION_X, PADDING_CALCULATION_X), pady=(100, 10))
 
 
 def main():
@@ -55,11 +41,11 @@ def main():
 
 
 if __name__ == '__main__':
+    main()
+    foo
     test_arr = make_columns(5, len(WORD), WORD)
-    print(test_arr[1][1].check("abcde"))
+    print(test_arr[1][1].color)
     print()
     print(test_arr[0])
     print()
     print(test_row(test_arr, 5, "abcde"))
-
-    main()
